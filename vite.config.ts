d4f5b713a8a6121de01625774d05abcab92bfa22
BrahnaJyoji <brahnajyoji@gmail.com>
2026-07-26 10:53:39 +0530

    Add Google Search Console verification

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-     base: '/Fancyfont/',
+     base: '/FancyFont/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
